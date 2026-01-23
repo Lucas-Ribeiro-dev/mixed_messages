58845cdb40688455a72276fe29297962e54a5216
Lucas-Ribeiro-dev <lucasismaeldirecional@gmail.com>
2026-01-23 10:16:07 -0300

    fixed the words that arent the first on the sentence to be on lowercase.

--- main.js
+++ main.js
@@ -1,24 +1,5 @@
-
-<<<<<<< HEAD
 //This one will give us a random number so we can reuse this funtion on our other functions
-=======
->>>>>>> refs/remotes/origin/main
 const randNum = () => Math.floor(Math.random() * 5) + 1;
-
-//generate the words to be used in the sentences
-const getWords = () => {
-    const group1 = ['Silence', 'Courage', 'Time', 'Fear', 'Hope']
-    const group2 = ['the unknown', 'the end', 'the horizon', 'the darkness', 'the truth'];
-
-    let wordOne = group1[randNum() - 1];
-    let wordTwo = group2[randNum() - 1];
-
-    return [wordOne, wordTwo];
-}
-
-const result = getWords();
-const wordOne = result[0];
-const wordTwo = result[1];
 
 //generate the words to be used in the sentences
 const getWords = () => {
@@ -40,16 +21,16 @@ const wordTwo = result[1];
 const getSentence = (num) => {
     switch (num) {
         case 1:
-            return `Between ${wordOne} and ${wordTwo}, there is a choice only you can make.`;
+            return `Between ${wordOne.toLowerCase()} and ${wordTwo}, there is a choice only you can make.`;
             break;
         case 2:
             return `${wordOne} is what remains when you face ${wordTwo}.`;
             break;
         case 3:
-            return `Sometimes, ${wordOne} appears just before ${wordTwo}.`;
+            return `Sometimes, ${wordOne.toLowerCase()} appears just before ${wordTwo}.`;
             break;
         case 4:
-            return `Without ${wordOne}, no step leads to ${wordTwo}.`
+            return `Without ${wordOne.toLowerCase()}, no step leads to ${wordTwo}.`
             break;
         case 5:
             return `${wordOne} doesn’t stop ${wordTwo}, but it changes how you reach it.`
